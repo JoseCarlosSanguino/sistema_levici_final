ddd6284aef893010f860466c39966bd30262d991
Jose Carlos Sanguino Muñoz <krostan9@gmail.com>
2023-03-22 20:17:21 +0100

    Create cambio de fuente  y formateo de numeros listado facturas

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -42,7 +42,7 @@
                                     <td>{{ $sale->operation->operationtype->groupoperationtype->abrev .'-' . $sale->operation->operationtype->letter . $sale->operation->FullNumber}}</td>
                                     <td>{{ $sale->operation->date_of }}</td>
                                     <td>{{ $sale->customer->name or '' }}</td>
-                                    <td>{{ ($sale->operation->operationtype_id == 15 || $sale->operation->operationtype_id == 16)?$sale->operation->amount*-1:$sale->operation->amount  }}</td>
+                                    <td>{{ ($sale->operation->operationtype_id == 15 || $sale->operation->operationtype_id == 16)?number_format($sale->operation->amount*-1,2,",","."):number_format($sale->operation->amount,2,",",".")  }}</td>
                                     <td>{{ $sale->operation->status->status or '' }}</td>
                                     <td>
                                         <a href="{{ url('/facturapdf/' . $sale->id) }}" target="_blank" title="Ver {{$modelName}}"><button class="btn btn-info btn-sm"><i class="fa fa-print" aria-hidden="true"></i> Imprimir</button></a>
